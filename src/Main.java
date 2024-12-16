d9421e96f46b47bcbba6fb091da7a4b8249f62fb
Felipe Fransa <antoniofelipesilva@live.com>
2024-12-16 16:42:08 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+        System.out.println("Movie: Avatar");
+
+        int movieAge = 2022;
+        double noteOne = 8.1;
+        double noteTwo = 4.7;
+        double noteThree = 6.0;
+
+        int classificationMovie = (int) ((noteOne + noteTwo + noteThree) /3);
+
+        System.out.println("Movie: Avatar" + "came out at the cinema in " +movieAge);
+        System.out.println("Movie: Avatar, critic's note " +classificationMovie);
+    }
+}
