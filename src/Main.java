57b2c268ecb1757bb82a68ff19094ca6c9ea02f5
Felipe Fransa <antoniofelipesilva@live.com>
2025-01-01 17:31:46 -0300

    add scanner reading

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner reading = new Scanner(System.in);
         LocalDate myDate = LocalDate.now();
+
+        System.out.println("enter your movie:");
+        String newMovie = reading.nextLine();
         System.out.println("Welcome");
-        System.out.println("Movie: Avatar");
 
         int movieAge = 2022;
         double noteOne = 8.1;
@@ -15,7 +17,9 @@ public class Main {
 
         int classificationMovie = (int) ((noteOne + noteTwo + noteThree) /3);
 
-        System.out.println("Movie: Avatar" + "came out at the cinema in " +movieAge);
+        System.out.println("Movie: " + newMovie + "came out at the cinema in " +movieAge);
         System.out.println("Movie: Avatar, critic's note " +classificationMovie);
+        System.out.println(myDate);
+        System.out.println();
     }
 }
