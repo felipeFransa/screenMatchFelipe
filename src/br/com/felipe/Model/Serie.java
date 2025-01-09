6e7d118ca1f8dcff99a3ec965cd882cec3938bda
Felipe Fransa <antoniofelipesilva@live.com>
2025-01-09 17:09:56 -0300

    class serie

---
+++ src/br/com/felipe/Model/Serie.java
@@ -0,0 +1,31 @@
+package br.com.felipe.Model;
+
+public class Serie {
+    private String title;
+    private int age;
+    private boolean typePlan;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isTypePlan() {
+        return typePlan;
+    }
+
+    public void setTypePlan(boolean typePlan) {
+        this.typePlan = typePlan;
+    }
+}
