e626c1bee091501db5fce50e763ca31e6f2a0b0c
Felipe Fransa <antoniofelipesilva@live.com>
2025-01-10 17:06:13 -0300

    add scanner serie title

--- src/br/com/felipe/Main/MainSerie.java
+++ src/br/com/felipe/Main/MainSerie.java
@@ -2,11 +2,16 @@ package br.com.felipe.Main;
 
 import br.com.felipe.Model.Serie;
 
+import java.util.Scanner;
+
 public class MainSerie {
     public static void main(String[] args) {
         Serie serie = new Serie();
+        Scanner reading = new Scanner(System.in);
+        System.out.println("Enter your serie: ");
+        String serietitle = reading.nextLine();
 
-        serie.setTitle("supernatural");
+        serie.setTitle(serietitle);
         serie.setAge(2003);
         serie.setTypePlan(true);
         serie.setSeasons(15);
