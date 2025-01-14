71c71b3f0f2226a364724f8592b0b8f3af256376
Felipe Fransa <antoniofelipesilva@live.com>
2025-01-14 16:19:53 -0300

    new movie reading scanner

--- src/br/com/felipe/Main/MainMovie.java
+++ src/br/com/felipe/Main/MainMovie.java
@@ -7,14 +7,17 @@ import java.util.Scanner;
 public class MainMovie {
     public static void main(String[] args) {
         Scanner reading = new Scanner(System.in);
+        System.out.println("Enter your movie:");
+        String readingMovie = reading.nextLine();
         Movie movie = new Movie();
 
-        movie.setFirstName("Avatar");
+        movie.setFirstName(readingMovie);
         movie.setAge(2022);
         movie.setTypPlan(true);
 
         if (movie.isTypPlan()){
             System.out.println(movie.getFirstName());
+            System.out.println(movie.getAge());
         }else {
             System.out.println("erro:");
         }
