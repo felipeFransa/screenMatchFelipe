0c56a25fbacdeee0357406074b172d7cafa1db79
Felipe Fransa <antoniofelipesilva@live.com>
2025-01-15 16:47:44 -0300

    add get/set description

--- src/br/com/felipe/Model/Movie.java
+++ src/br/com/felipe/Model/Movie.java
@@ -3,6 +3,15 @@ package br.com.felipe.Model;
 public class Movie {
     private String firstName;
     private int age;
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public boolean isTypPlan() {
         return typPlan;
