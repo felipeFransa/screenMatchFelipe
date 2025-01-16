0312692d350715076798ad5e04d2da4e1f6bf505
Felipe Fransa <antoniofelipesilva@live.com>
2025-01-16 18:11:45 -0300

    add if/else main

--- src/br/com/felipe/Main/Main.java
+++ src/br/com/felipe/Main/Main.java
@@ -3,12 +3,14 @@ package br.com.felipe.Main;
 import br.com.felipe.Model.Movie;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner reading = new Scanner(System.in);
         LocalDate myDate = LocalDate.now();
+        LocalTime myTime = LocalTime.now();
         Movie movie = new Movie();
 
         System.out.println("enter your movie:");
@@ -18,6 +20,7 @@ public class Main {
         movie.setFirstName(newMovie);
         movie.setAge(2022);
         movie.setNote(8.2);
+        movie.setTypPlan(true);
 
         double noteOne = movie.getNote();
         double noteTwo = 4.7;
@@ -25,9 +28,14 @@ public class Main {
 
         int classificationMovie = (int) ((noteOne + noteTwo + noteThree) /3);
 
-        System.out.println("Movie: " + newMovie + "came out at the cinema in " +movie.getFirstName());
-        System.out.println("Movie: Avatar, critic's note " +classificationMovie);
-        System.out.println(movie.getAge());
-        System.out.println(myDate);
+        if (movie.isTypPlan()){
+            System.out.println("Movie: " + newMovie + "came out at the cinema in " +movie.getFirstName());
+            System.out.println("Movie: Avatar, critic's note " +classificationMovie);
+            System.out.println(movie.getAge());
+            System.out.println(myDate);
+        }else {
+            System.out.println("error:");
+        }
+
     }
 }
