4b7d589ddbc580d874b74e73c737f4261239dea6
Felipe Fransa <antoniofelipesilva@live.com>
2025-01-19 20:49:24 -0300

    client class

---
+++ src/br/com/felipe/Model/Client.java
@@ -0,0 +1,31 @@
+package br.com.felipe.Model;
+
+public class Client {
+    private String firstName;
+    private Integer age;
+    private String city;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
