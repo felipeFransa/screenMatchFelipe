dc29385f490f402a7d79908ce8db07f743a4dfef
Felipe Fransa <antoniofelipesilva@live.com>
2025-01-20 17:01:14 -0300

    main register client

---
+++ src/br/com/felipe/Main/MainRegister.java
@@ -0,0 +1,9 @@
+package br.com.felipe.Main;
+
+import br.com.felipe.Model.Client;
+
+public class MainRegister {
+    public static void main(String[] args) {
+        Client registerClient = new Client();
+    }
+}
