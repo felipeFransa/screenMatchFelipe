05e4638e7806b89b90c45b6ca90e3d4ec128b3fe
Felipe Fransa <antoniofelipesilva@live.com>
2025-01-21 18:00:38 -0300

    main register print set/get

--- src/br/com/felipe/Main/MainRegister.java
+++ src/br/com/felipe/Main/MainRegister.java
@@ -5,5 +5,13 @@ import br.com.felipe.Model.Client;
 public class MainRegister {
     public static void main(String[] args) {
         Client registerClient = new Client();
+
+        registerClient.setFirstName("");
+        registerClient.setAge(27);
+        registerClient.setCity("");
+
+        System.out.println(registerClient.getFirstName());
+        System.out.println(registerClient.getAge());
+        System.out.println(registerClient.getCity());
     }
 }
