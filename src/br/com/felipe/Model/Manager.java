f6f5232b26296bc677c8429527a9e74779dbea2b
Felipe Fransa <antoniofelipesilva@live.com>
2025-01-25 19:46:42 -0300

    add manager 2

--- src/br/com/felipe/Model/Manager.java
+++ src/br/com/felipe/Model/Manager.java
@@ -3,4 +3,20 @@ package br.com.felipe.Model;
 public class Manager {
     private String firstName;
     private Integer age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
