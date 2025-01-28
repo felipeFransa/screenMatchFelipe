638622ace9651fb2c1d4e8739f5bfc46b4ab51cf
Felipe Fransa <antoniofelipesilva@live.com>
2025-01-28 18:42:53 -0300

    main manager

---
+++ src/br/com/felipe/Main/MainManager.java
@@ -0,0 +1,9 @@
+package br.com.felipe.Main;
+
+import br.com.felipe.Model.Manager;
+
+public class MainManager {
+    public static void main(String[] args) {
+        Manager manager = new Manager();
+    }
+}
