2411d2c9cb2ddd5beef801c02bb2e10f1c6d5531
Felipe Fransa <antoniofelipesilva@live.com>
2025-01-29 18:20:25 -0300

    main manager reading

--- src/br/com/felipe/Main/MainManager.java
+++ src/br/com/felipe/Main/MainManager.java
@@ -2,8 +2,13 @@ package br.com.felipe.Main;
 
 import br.com.felipe.Model.Manager;
 
+import java.time.LocalTime;
+import java.util.Scanner;
+
 public class MainManager {
     public static void main(String[] args) {
         Manager manager = new Manager();
+        Scanner reading = new Scanner(System.in);
+        LocalTime myTime = LocalTime.now();
     }
 }
